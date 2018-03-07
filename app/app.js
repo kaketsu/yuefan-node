b9ed2554c70726fa88161d4461d19bd55cdd5644
jiayin <jiayin@chenhaodeMacBook-Pro.local>
2018-03-07 21:58:44 +0800

    enable mysql

--- app/app.js
+++ app/app.js
@@ -1,13 +1,20 @@
 const koa = require('koa');
 const logger = require('koa-logger');
 const bodyParser = require('koa-bodyparser');
-const koaRouter = require('./api/api');
-const mongoConnect = require('./db/mongooseConnect');
+// const koaRouter = require('./api/api');
+// const mongoConnect = require('./db/mongooseConnect');
+// const mysqlConnect = require('./db/mysqlConnect');
 
-mongoConnect();
+const koaRouter = require('./api/router');
+
 const app = new koa();
 app.use(logger());
 app.use(bodyParser());
 app.use(koaRouter.routes());
+
+app.use( async (ctx) => {
+    console.log(ctx);
+    console.log(ctx.request);
+});
 
 app.listen(3000);
